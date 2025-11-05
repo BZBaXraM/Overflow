7442604daeecd142741da33eee07f417d0b0351f
BahramTron97 <b.bayramzada@hebbcloud.com>
2025-11-05 14:51:40 +0400

    next.js app added with hero ui

---
+++ webapp/app/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { Button } from "@heroui/button";
+import { AcademicCapIcon } from "@heroicons/react/24/solid";
+
+export default function Home() {
+	return (
+		<div>
+			<h1>Overflow app</h1>
+			{/* <Button
+				color="primary"
+				endContent={<AcademicCapIcon className="size-6" />}
+			>
+				Click me!
+			</Button> */}
+		</div>
+	);
+}
